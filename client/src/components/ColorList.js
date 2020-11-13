7c35022ccef609409f9d1b0658b0aa9952f885cf
Alden Ho <aldenho@Aldens-MacBook-Air.local>
2020-11-13 10:01:14 -0700

    edit button working

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -22,6 +22,19 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+    .put(`/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        console.log(res)
+        updateColors(
+          ...colors,
+          colorToEdit
+        )
+        setEditing(false)
+      })
+      .catch(err => {
+        console.log(err)
+    })
   };
 
   const deleteColor = color => {
