872f414fe004753a53a5b367501827cd79266981
Alden Ho <aldenho@Aldens-MacBook-Air.local>
2020-11-13 09:24:06 -0700

    added login page with token and authentication, axios post

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
-const Login = () => {
+const initialLoginInfo = {
+  username: '',
+  password: ''
+}
+
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [loginInfo, setLoginInfo] = useState(initialLoginInfo)
+
+  const handleChange = e => {
+    setLoginInfo({
+        ...loginInfo,
+        [e.target.name]: e.target.value
+      });
+  };
+
+  const login = e => {
+    e.preventDefault();
+    axios.post('http://localhost:5000/api/login', loginInfo)
+    .then(res => {
+      localStorage.setItem("token", res.data.payload);
+      props.history.push("/protected");
+      props.setIsLoggedIn(true);
+    })
+    .catch( err => {
+      console.log(err)
+    });
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <div>
+      <form onSubmit={login}>
+        <input
+          type="text"
+          name="username"
+          value={loginInfo.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={loginInfo.password}
+          onChange={handleChange}
+        />
+        <button>Log in</button>
+      </form>
+    </div>
     </>
   );
 };
