a8709b5fd4ae747f166c4ec6635036a7ccaa9963
Alden Ho <aldenho@Aldens-MacBook-Air.local>
2020-11-13 11:02:59 -0700

    added testing, colors rendering properly to bubblePage. SPRINT COMPLETED

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,44 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import BubblePage from "./BubblePage";
+import { fetchData as mockFetchData } from '../api/fetchData'
+jest.mock('../api/fetchData')
 
-test("Fetches data and renders the bubbles", () => {
-  // Finish this test
+let colors = [
+  {
+    color: "aliceblue",
+    code: {
+      hex: "#f0f8ff"
+    },
+    id: 1
+  },
+  {
+    color: "limegreen",
+    code: {
+      hex: "#99ddbc"
+    },
+    id: 2
+  },
+]
+
+test("Fetches data and renders the bubbles", async () => {
+  // mockFetchData.mockResolvedValueOnce(colors)
+  const {rerender} = render(<BubblePage colorList={[]}/>)
+
+  // page with headers rendering properly
+  const colorHeader = screen.getByText(/colors/i)
+  const bubblesHeader = screen.getByText(/bubbles/i)
+
+  // let colorObjects = screen.queryAllByTestId('color')
+  // expect(colorObjects).toStrictEqual([])
+
+  rerender(<BubblePage colorList={colors}/>)
+
+  await waitFor(() => screen.queryByText(/alice/i))
+
+  const limegreen= screen.queryByText(/lime/i)
+
+  // colorObjects = screen.queryAllByTestId('color')
+  // expect(colorObjects).toHaveLength(2)
+
 });
