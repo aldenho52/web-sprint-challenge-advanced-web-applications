10af3cba6b89493ca86d295d94b96a26f6ae33ce
Alden Ho <aldenho@Aldens-MacBook-Air.local>
2020-11-13 11:12:57 -0700

    updated testing

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -4,26 +4,26 @@ import BubblePage from "./BubblePage";
 import { fetchData as mockFetchData } from '../api/fetchData'
 jest.mock('../api/fetchData')
 
-let colors = [
-  {
-    color: "aliceblue",
-    code: {
-      hex: "#f0f8ff"
-    },
-    id: 1
-  },
-  {
-    color: "limegreen",
-    code: {
-      hex: "#99ddbc"
-    },
-    id: 2
-  },
-]
+// let colors = [
+//   {
+//     color: "aliceblue",
+//     code: {
+//       hex: "#f0f8ff"
+//     },
+//     id: 1
+//   },
+//   {
+//     color: "limegreen",
+//     code: {
+//       hex: "#99ddbc"
+//     },
+//     id: 2
+//   },
+// ]
 
-test("Fetches data and renders the bubbles", async () => {
+test("Fetches api data and renders the bubbles", async () => {
   // mockFetchData.mockResolvedValueOnce(colors)
-  const {rerender} = render(<BubblePage colorList={[]}/>)
+  const {rerender} = render(<BubblePage/>)
 
   // page with headers rendering properly
   const colorHeader = screen.getByText(/colors/i)
@@ -32,13 +32,11 @@ test("Fetches data and renders the bubbles", async () => {
   // let colorObjects = screen.queryAllByTestId('color')
   // expect(colorObjects).toStrictEqual([])
 
-  rerender(<BubblePage colorList={colors}/>)
-
+  rerender(<BubblePage/>)
+  
+  // colors from API rendering
   await waitFor(() => screen.queryByText(/alice/i))
-
   const limegreen= screen.queryByText(/lime/i)
 
-  // colorObjects = screen.queryAllByTestId('color')
-  // expect(colorObjects).toHaveLength(2)
 
 });
